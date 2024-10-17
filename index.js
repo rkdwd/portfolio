2e67a0a548ca939e08b6dc9627833955e0cfcff8
rkdwd <159889372+rkdwd@users.noreply.github.com>
2024-10-17 21:57:28 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-window.addEventListener('load', function () {
-    var allElements = document.getElementsByTagName('*');
-    Array.prototype.forEach.call(allElements, function (el) {
-      var includePath = el.dataset.includePath;
-      if (includePath) {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-          if (this.readyState == 4 && this.status == 200) {
-            el.outerHTML = this.responseText;
-          }
-        };
-        xhttp.open('GET', includePath, true);
-        xhttp.send();
-      }
-    });
-  });
+let modals = new Array(12);
+let imgs = new Array(12);
+let modalImgs = new Array(12);
+let captionTexts = new Array(12);
+let spans = new Array(12);
+
+for(let i=1; i<=12; i++){
+  modals[i-1] = document.getElementById("modal"+String(i));
+  imgs[i-1] = document.getElementById("myImg"+String(i));
+
+  modalImgs[i-1] = document.getElementById("modalImg"+String(i));
+  captionTexts[i-1] = document.getElementById("caption"+String(i));
+
+  imgs[i-1].onclick = function(){
+    modals[i-1].style.display = "block";
+    modalImgs[i-1].src = this.src;
+    captionTexts[i-1].innerHTML = this.alt;
+  }
+
+  spans[i-1] = document.getElementById("close"+String(i));
+
+  spans[i-1].onclick = function(){
+    modals[i-1].style.display = "none";
+  }
+}
